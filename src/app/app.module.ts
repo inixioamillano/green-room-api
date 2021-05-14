60a2c81d97679a274a0042956227cfc1ef6bb713
Inixio <inixio97@gmail.com>
2021-05-14 18:06:24 +0200

    Add heroku config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,11 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'greenroom',
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'root',
+      database: process.env.DB_NAME || 'greenroom',
       entities: [Contestant, User, Vote, Room, Config],
       synchronize: true,
     }),
